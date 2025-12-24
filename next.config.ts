465435cfd2c53dcee399f46221b4600314a4460f
tackle063444 <tuzkides@gmail.com>
2025-12-24 14:20:50 +0700

    fix: :bug: remove eslint config from next.config.ts to fix build error

--- next.config.ts
+++ next.config.ts
@@ -6,11 +6,6 @@ const nextConfig: NextConfig = {
 	images: {
 		unoptimized: true,
 	},
-	// ปิดการใช้งาน server-side features บางอย่างที่ไม่รองรับใน Static Export ถ้าจำเป็น
-	eslint: {
-		// ปิดการเช็ค lint ตอน build เพื่อป้องกัน build failed จาก warning เล็กน้อย
-		ignoreDuringBuilds: true,
-	},
 };
 
 export default nextConfig;
